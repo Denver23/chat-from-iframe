b2d9fec11a802124cc76a4d31dab5a39cadd2b8a
Denis Dunaievskiy <denver9523@gmail.com>
2022-04-29 12:24:49 +0300

    example of working react build inside express app

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const api = require("./api");
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/", express.static(__dirname + "/public/build"));
+app.use("/chat", express.static(__dirname + "/public/build"));
+app.use("/", express.static(__dirname + "/public"));
 app.use(api);
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, '/public/', 'index.html'));
+});
+
+app.get("/chat/*", function (req, res) {
+  console.log(path.join(__dirname, '/public/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/public/build', 'index.html'));
+});
+
 module.exports = app;
--- public/src/App.tsx
+++ public/src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={'/test'}>
       <div className="App">
         <header className="App-header">
           <div className="nav">
